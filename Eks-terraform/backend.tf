24483d683e52ba85bf7c4e2c0a33f02576640c94
adrifok <adrifok@gmail.com>
2024-01-06 15:08:21 -0300

    update ec2 name

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "candy-crush-ec2" # Replace with your actual S3 bucket name
+    bucket = "three-tier-ec2" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1b"
   }
