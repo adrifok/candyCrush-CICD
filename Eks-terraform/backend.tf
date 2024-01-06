76edd644f41a8388752ea41e294196080d6b7f2d
adrifok <adrifok@gmail.com>
2024-01-06 16:17:53 -0300

    update to aws us-east-1

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "three-tier-ec2" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1b"
+    region = "us-east-1"
   }
 }
